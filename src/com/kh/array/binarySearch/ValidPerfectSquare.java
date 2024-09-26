0d13c14fe2f7953d9889191925b9de4772c1acfc
kanghao <kanghao@leagsoft.com>
2024-09-26 16:32:05 +0800

    367. Valid Perfect Square solution

--- src/com/kh/array/binarySearch/ValidPerfectSquare.java
+++ src/com/kh/array/binarySearch/ValidPerfectSquare.java
@@ -7,33 +7,42 @@ package com.kh.array.binarySearch;
  * @Date 2024/9/26 15:01
  * @Version 1.0
  **/
-public class ValidPerfactSquare {
-
+public class ValidPerfectSquare {
+    // Time Complexity O(logn)
+    // Space complexity  O(1)
     public static boolean getPerfectSquare(int positiveNum) {
-        if(positiveNum <= 0) {
+        if (positiveNum < 0 || positiveNum > 1L << 32 - 1) {
             return false;
         }
 
+        if (1 == positiveNum) {
+            return true;
+        }
+
         int left = 0;
-        int right = positiveNum;
-        while (left <= right) {
+        int right = positiveNum / 2;
+        while (left < right) {
             int middle = left + (right - left) / 2;
-            if (middle * middle > positiveNum) {
-                right = middle - 1;
-            } else if (middle * middle < positiveNum) {
+            if (middle < positiveNum * 1.0 / middle) {
                 left = middle + 1;
             } else {
-                return true;
+                right = middle;
             }
         }
-        return false;
+        return right == positiveNum * 1.0 / right;
     }
+
     public static void main(String[] args) {
-        int num = 16;
+        int num = 808201;
         System.out.println("Is num a valid perfect square: " + getPerfectSquare(num));
 
         int num1 = 14;
         System.out.println("Is num1 a valid perfect square: " + getPerfectSquare(num1));
-    }
 
+        int num2 = 2147483647;
+        System.out.println("Is num2 a valid perfect square: " + getPerfectSquare(num2));
+
+        int num3 = 1;
+        System.out.println("Is num3 a valid perfect square: " + getPerfectSquare(num3));
+    }
 }
